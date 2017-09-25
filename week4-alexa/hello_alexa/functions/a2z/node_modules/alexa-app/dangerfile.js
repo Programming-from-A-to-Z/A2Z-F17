2a5702ee4e0406c7ff5727362d3df1de2b3f2727
Daniel Shiffman <daniel@shiffman.net>
2017-09-25 14:15:16 -0400

    node modules oops

--- week4-alexa/hello_alexa/functions/a2z/node_modules/alexa-app/dangerfile.js
+++
@@ -1,7 +0,0 @@
-import { danger, warn } from "danger"
-
-// did you forget to update changelog?
-const hasChangelog = danger.git.modified_files.includes("CHANGELOG.md");
-if (! hasChangelog) {
-  warn("Did you forget to update CHANGELOG.md?");
-}
--- week4-alexa/hello_alexa/functions/a2z/node_modules/alexa-app/lib/normalize-api-path.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var path = require('path');
-
-module.exports = function normalizeApiPath(apiPath) {
-  return path.posix.normalize(path.posix.join('/', apiPath));
-};
